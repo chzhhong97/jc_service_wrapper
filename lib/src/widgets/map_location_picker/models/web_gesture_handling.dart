3f1a3900ffceba4fb51da09a1454cc8208451f98
Chong Zheng Hong <zhchong@cmg.com.my>
2025-09-24 12:37:12 +0800

    -push missing files

---
+++ lib/src/widgets/map_location_picker/models/web_gesture_handling.dart
@@ -0,0 +1,24 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart' as gm show WebGestureHandling;
+
+/// This setting controls how the API handles gestures on the map
+enum WebGestureHandling {
+  /// Scroll events and one-finger touch gestures scroll the page, and do not
+  /// zoom or pan the map. Two-finger touch gestures pan and zoom the map.
+  /// Scroll events with a ctrl key or ⌘ key pressed zoom the map. In this mode
+  /// the map cooperates with the page.
+  cooperative,
+
+  /// All touch gestures and scroll events pan or zoom the map.
+  greedy,
+
+  /// The map cannot be panned or zoomed by user gestures.
+  none,
+
+  /// (default) Gesture handling is either cooperative or greedy, depending on
+  /// whether the page is scrollable or in an iframe.
+  auto;
+
+  gm.WebGestureHandling toGoogle(){
+    return gm.WebGestureHandling.values.firstWhere((e) => e.name == name, orElse: () => gm.WebGestureHandling.auto);
+  }
+}
